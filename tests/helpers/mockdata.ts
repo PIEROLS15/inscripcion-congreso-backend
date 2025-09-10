b44ff15aa20fd09fee4e8932f14471ed4059f33e
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2025-09-09 20:21:07 -0500

    feat: test users

---
+++ tests/helpers/mockdata.ts
@@ -0,0 +1,11 @@
+export const mockUser = {
+    id: 1,
+    dni: '12345678',
+    nombres: 'Juan',
+    apellidos: 'Pérez',
+    correoElectronico: 'juan@example.com',
+    celular: '987654321',
+    creadoEn: new Date(),
+    actualizadoEn: new Date(),
+    inscripciones: [],
+}
