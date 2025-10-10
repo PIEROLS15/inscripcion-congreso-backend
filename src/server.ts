39205e2b08c61c00251b3a29f064455e0032fe38
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2025-10-10 12:35:15 -0500

    feat: send email

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app'
 import { env } from '../config/env'
 
 app.listen(env.PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server corriendo en http://0.0.0.0:${env.PORT}`)
+  console.log(`🚀 Server corriendo en http://localhost:${env.PORT}`)
 })
